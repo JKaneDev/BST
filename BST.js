d59174d9124e52492c74c7f277914b44be364a54
James Kane <james.kane59@outlook.com>
2023-01-11 19:29:50 +0000

    fixing buildTree bugs, this keyword was missing, correct val was not passed to node

--- BST.js
+++ BST.js
@@ -1,6 +1,6 @@
 class Node {
-	constructor() {
-		this.val = null;
+	constructor(val) {
+		this.val = val;
 		this.left = null;
 		this.right = null;
 	}
@@ -8,25 +8,41 @@ class Node {
 
 class BST {
 	constructor(array) {
-		this.root = null;
 		this.startIndex = 0;
 		this.endIndex = array.length - 1;
-
-		this.buildTree(array, this.startIndex, this.endIndex);
+		this.root = this.buildTree(array, this.startIndex, this.endIndex);
 	}
 
 	buildTree(array, startIndex, endIndex) {
 		// base case: if start index is greater than end index
 		if (startIndex > endIndex) return null;
 		// calculate middle of array
-		let middle = startIndex + endIndex / 2;
+		let middle = Math.floor((startIndex + endIndex) / 2);
 		// create new node
 		let newNode = new Node(array[middle]);
 		// recursively call create tree on left node
-		newNode.left = createTree(array, start, middle - 1);
+		newNode.left = this.buildTree(array, startIndex, middle - 1);
 		// recursively call create tree on right node
-		newNode.right = createTree(array, middle + 1, end);
+		newNode.right = this.buildTree(array, middle + 1, endIndex);
 
-		return this;
+		return newNode;
+	}
+
+	prettyPrint(node, prefix = '', isLeft = true) {
+		if (!node) return;
+		if (node.right !== null) {
+			this.prettyPrint(
+				node.right,
+				`${prefix}${isLeft ? '│   ' : '    '}`,
+				false
+			);
+		}
+		console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.val}`);
+		if (node.left !== null) {
+			this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+		}
 	}
 }
+
+const newTree = new BST([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+newTree.prettyPrint(newTree.root);
