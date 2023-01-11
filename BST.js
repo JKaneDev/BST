789006fad96b678b5743b517b1f6d6b4a3e3895c
James Kane <james.kane59@outlook.com>
2023-01-11 20:00:52 +0000

    writing pseudocode for insert function

--- BST.js
+++ BST.js
@@ -42,6 +42,17 @@ class BST {
 			this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
 		}
 	}
+
+	insert(val, currentNode = this.root) {
+		// if root is null, create new node at root
+		// else compare val to root val
+		// if val is less than current node val, check for left node, if no left node, set newNode here
+		// else if there is a left node, call insert again
+		// if val is greater than root val, check for right node, if no right node, set newNode here
+		// else if there is a right node, call insert again
+		// if val is equal to current val, return undefined to prevent duplication
+		// return tree
+	}
 }
 
 const newTree = new BST([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
